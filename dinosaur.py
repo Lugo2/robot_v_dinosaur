0e250b89f2ecbb546f0ea2fd0d524e72870d1064
Mario Lugo <mlugo23699@gmail.com>
2022-08-12 14:29:25 -0700

    gave arguments to weapon, dinosaur, and robot initializer

--- dinosaur.py
+++ dinosaur.py
@@ -1,3 +1,10 @@
+from robot import Robot
 class Dinosaur:
-    def __init__(self):
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attack_power = attack_power
+        self.health = 100
+
+    def attack(self, robot):
         pass
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from battlefield import Battleflield
+
+battle_field = Battleflield()
+battle_field
